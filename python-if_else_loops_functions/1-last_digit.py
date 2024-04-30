60f84325c0e7f4217252b7e7ee3f63428a9d3f1a
EliBrank <eliasbrinkman@gmail.com>
2024-04-29 22:44:48 -0500

    develop pos/neg and last digit scripts

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def last_digit(n):
+    return abs(n) % 10
+import random
+number = random.randint(-10000, 10000)
+
+print(f"Last digit of {number} is {last_digit(number)}", end='')
+
+if last_digit(number) > 5:
+    print(" and is greater than 5")
+elif last_digit(number) == 0:
+    print(" and is 0")
+elif last_digit(number) < 6:
+    print(" and is less than 6 and not 0")
