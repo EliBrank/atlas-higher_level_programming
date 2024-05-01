65948df793994ccc985d4a5aaf48f504784905fb
EliBrank <eliasbrinkman@gmail.com>
2024-04-30 23:42:35 -0500

    fix print statement for int comb printer

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-for i in range (10):
-    for j in range (10):
+first_num = True
+
+for i in range(10):
+    for j in range(10):
         if (i < j):
-            print("{}{}".format(str(i), str(j)))
+            if first_num is True:
+                print("{}{}".format(str(i), str(j)), end='')
+                first_num = False
+            else:
+                print(", {}{}".format(str(i), str(j)), end='')
+print("")
