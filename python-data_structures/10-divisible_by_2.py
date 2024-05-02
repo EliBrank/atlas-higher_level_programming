3b65a440d9e0f185d5c93f9458c92c5de9cb26b5
EliBrank <eliasbrinkman@gmail.com>
2024-05-02 17:54:33 -0500

    create check even func

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    div_two_list = my_list[:]
+    for i in range(len(my_list)):
+        if abs(my_list[i]) % 2 == 0:
+            div_two_list[i] = True
+        else:
+            div_two_list[i] = False
+    return div_two_list
