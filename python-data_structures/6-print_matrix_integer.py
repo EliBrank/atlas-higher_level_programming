e050a3238a550880376882bf9e38f1f1866489d9
EliBrank <eliasbrinkman@gmail.com>
2024-05-02 16:10:53 -0500

    create matrix print, tuple add, and multiple return funcs

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        first_in_row = True
+        for i in row:
+            if first_in_row:
+                print("{:d}".format(i), end='')
+                first_in_row = False
+            else:
+                print(" {:d}".format(i), end='')
+        print("")
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_1, a_2, b_1, b_2 = 0, 0, 0, 0
+
+    if len(tuple_a) >= 1:
+        a_1 = tuple_a[0]
+        if len(tuple_a) >= 2:
+            a_2 = tuple_a[1]
+
+    if len(tuple_b) >= 1:
+        b_1 = tuple_b[0]
+        if len(tuple_b) >= 2:
+            b_2 = tuple_b[1]
+
+    c = a_1 + b_1
+    d = a_2 + b_2
+
+    new_tuple = (c, d)
+    return new_tuple
