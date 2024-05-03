fd90539b985ee880f2840e61309ae42ea41c7b9f
EliBrank <eliasbrinkman@gmail.com>
2024-05-03 13:58:39 -0500

    add search replace, add unique, and find common element funcs

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    while True:
+        try:
+            idx = new_list.index(search)
+            new_list[idx] = replace
+        except ValueError:
+            break
+    return new_list
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    my_set = set(my_list)
+    uniq_list = list(my_set)
+    list_sum = 0
+    for i in uniq_list:
+        list_sum += i
+    return list_sum
