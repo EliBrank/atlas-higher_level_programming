44570b3fbb5ec0dc35c7c1f91056dc0888a5a198
EliBrank <eliasbrinkman@gmail.com>
2024-05-03 16:19:17 -0500

    create mult by 2, delete key, dict update, max score, and map funcs, add gitignore

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) < 1:
+        return None
+    max_value = max(a_dictionary.keys())
+    return max_value
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    two_dict = a_dictionary.copy()
+    for i in two_dict:
+        two_dict[i] *= 2
+    return two_dict
