7ea129d780a39888436baab4887590b7d5a372d8
EliBrank <eliasbrinkman@gmail.com>
2024-05-03 15:12:46 -0500

    make difference checker, num key checker, dict sorter funcs

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    # Creates a new list of keys in alphabetical order
+    sorted_keys = sorted(a_dictionary)
+    for key in sorted_keys:
+        # Accessing dict at [key] is like accessing list at [index]
+        print("{}: {}".format(key, a_dictionary[key]))
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
