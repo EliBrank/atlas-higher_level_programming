32f16b212f762497f80143bed023f708c1d2dfed
EliBrank <eliasbrinkman@gmail.com>
2024-05-03 13:28:54 -0500

    create square matrix func using list comprehension

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    # List comprehension
+    # Inner list square each value for a row
+    # Outer list composed of each completed row
+    return [[x * x for x in row] for row in matrix]
