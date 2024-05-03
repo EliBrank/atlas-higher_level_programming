fb44ab4c0ed917fb85d303fa7c04ffd74953014f
EliBrank <eliasbrinkman@gmail.com>
2024-05-03 15:15:54 -0500

    remove test code

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,6 +6,3 @@ def print_sorted_dictionary(a_dictionary):
     for key in sorted_keys:
         # Accessing dict at [key] is like accessing list at [index]
         print("{}: {}".format(key, a_dictionary[key]))
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
