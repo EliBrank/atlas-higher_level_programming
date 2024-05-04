0f607c18813ed5a93043bc35dd5a5425651725f2
EliBrank <eliasbrinkman@gmail.com>
2024-05-04 18:33:53 -0500

    fix best score func to properly look at values instead of keys

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) < 1:
         return None
-    max_value = max(a_dictionary.keys())
+    max_value = max(a_dictionary, key=a_dictionary.get)
     return max_value
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    
+
+
+
+
+
+
+
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
