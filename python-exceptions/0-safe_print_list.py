b06f2e70d711b741fc5ae2e523b8a0f9d279b159
EliBrank <eliasbrinkman@gmail.com>
2024-05-05 13:25:18 -0500

    move newline print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,8 +6,18 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             print_ct += 1
-        print()
     except IndexError:
         pass
     finally:
+        print()
         return print_ct
+
+
+my_list = [1, 2, 3, 4]
+
+nb_print = safe_print_list(my_list, len(my_list) + 1)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, 0)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
