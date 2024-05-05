6bcdbe5c0316c2ae9c12f76277d3e5fa95d35524
EliBrank <eliasbrinkman@gmail.com>
2024-05-05 13:16:53 -0500

    re-work safe print list func

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 1
-    if x == 0:
-        return 0
+    print_ct = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
+            print_ct += 1
         print()
-        return i + 1
     except IndexError:
-        print()
-        return i
-
+        pass
+    finally:
+        return print_ct
 
 my_list = [4, 3, 1, 8, 6]
 
