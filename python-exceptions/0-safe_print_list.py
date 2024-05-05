4f94891949d03963b7402a938ce093c15f83a82d
EliBrank <eliasbrinkman@gmail.com>
2024-05-05 00:07:01 -0500

    make adjustments to print list func

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i, last_index = 0, 0
+    i = 1
     try:
         for i in range(x):
             print(my_list[i], end='')
-            last_index = i
+        if x > 0:
+            print()
+        return i + 1
     except IndexError:
-        return last_index
-    if x > 0:
         print()
-    return i
+        return i
