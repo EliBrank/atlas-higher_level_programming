8d1ddddec175bb3cb6661dd7ce16e2e547609665
EliBrank <eliasbrinkman@gmail.com>
2024-05-05 14:06:52 -0500

    add print list ints func

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    print_ct = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            print_ct += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+
+    return print_ct
