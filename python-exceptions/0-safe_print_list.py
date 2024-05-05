2ea73e910c9b98bcf606a626378f56182a78f90c
EliBrank <eliasbrinkman@gmail.com>
2024-05-05 13:26:02 -0500

    remove test code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -11,13 +11,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
         return print_ct
-
-
-my_list = [1, 2, 3, 4]
-
-nb_print = safe_print_list(my_list, len(my_list) + 1)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, 0)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
