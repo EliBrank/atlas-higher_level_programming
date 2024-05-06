bf2b3c8029b97c52c4d2ea0eae8099cf3b0420f7
EliBrank <eliasbrinkman@gmail.com>
2024-05-06 11:48:35 -0500

    wrap everything in try/except block

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is None or len(roman_string) < 1:
+    try:
+        if roman_string is None or len(roman_string) < 1:
+            return 0
+    except TypeError:
         return 0
-    numerals = {
-        'M': 1000,
-        'D': 500,
-        'C': 100,
-        'L': 50,
-        'X': 10,
-        'V': 5,
-        'I': 1
-    }
-    total = 0
+    finally:
+        numerals = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        total = 0
 
-    for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:
-            total += numerals[roman_string[i]]
-            break
-        elif numerals[roman_string[i]] < numerals[roman_string[i + 1]]:
-            total -= numerals[roman_string[i]]
-        else:
-            total += numerals[roman_string[i]]
+        for i in range(len(roman_string)):
+            if i == len(roman_string) - 1:
+                total += numerals[roman_string[i]]
+                break
+            elif numerals[roman_string[i]] < numerals[roman_string[i + 1]]:
+                total -= numerals[roman_string[i]]
+            else:
+                total += numerals[roman_string[i]]
 
-    return total
+        return total
