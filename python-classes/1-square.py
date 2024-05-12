a09214122bfc477c0172b8307c801b6c270352a0
EliBrank <eliasbrinkman@gmail.com>
2024-05-12 17:38:58 -0500

    add private attribute to square class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+"""establishes class Square
+
+    it is instantiated with the private attr size
+"""
+
+
+class Square:
+    """class is instatiated with private attr size"""
+    def __init__(self, size):
+        self.__size = size
