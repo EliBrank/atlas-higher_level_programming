60c306bbf1aaa8366c77d6a8afaa1c27cf51332a
EliBrank <eliasbrinkman@gmail.com>
2024-05-12 22:18:12 -0500

    work on squared square func

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+"""establishes class Square"""
+
+
+class Square:
+    """Square is instatiated with size"""
+    def __init__(self, size=0):
+        """defines size with instance
+
+        Args:
+            size (opt): side length of Square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates area of Square
+
+        Returns: area - size, squared
+        """
+
+        return self.__size**2
