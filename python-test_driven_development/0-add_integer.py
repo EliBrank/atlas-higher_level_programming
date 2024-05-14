c118533dee80a388308a7eeb3b519ae9b7101f9f
EliBrank <eliasbrinkman@gmail.com>
2024-05-14 17:13:28 -0500

    document add integer func, start working on matrix divide func

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""defines function add_integer"""
+
+
 def add_integer(a, b=98):
     """adds two integers together
     floats are typecast to ints
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""defines function matrix_divided"""
+
+
+def matrix_divided(matrix, div):
+    """divides each entry of matrix by a value
+
+    Args:
+        matrix: list of lists of values to be divided
+        div: divisor for matrix values
+
+    Returns: new matrix with divided values
+    """
+
+    for row in matrix:
+        for a in row:
+            if isinstance(a, float):
+                a = int(a)
+            if not isinstance(a, int):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    [[x / div for x in row] for row in matrix]
