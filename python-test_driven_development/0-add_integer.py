c69bca2ed6e086a7ed955a5f32da654e9b231554
EliBrank <eliasbrinkman@gmail.com>
2024-05-14 15:16:31 -0500

    work on function, doctests for add integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 
+def add_integer(a, b=98):
+    """adds two integers together
+    floats are typecast to ints
 
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns: sum of two integers
+    """
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return a + b
