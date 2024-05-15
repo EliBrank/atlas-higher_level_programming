bcfa452ec4fb111a11549198b309ef997be77de6
EliBrank <eliasbrinkman@gmail.com>
2024-05-14 21:31:07 -0500

    write print square function and try out tests

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+"""defines the function print_square"""
+
+
+def print_square(size):
+    """prints ASCII square composed of "#" to stdout
+
+    Args:
+        size: length of each side of square
+    """
+
+    if isinstance(size, float):
+        if size >= 0:
+            size = int(size)
+        else:
+            raise TypeError("size must be an integer")
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print()
+    for i in range(size):
+        for i in range(size):
+            print("#", end="")
+        print()
