0b02a605d3c94ec6579eab19517abe6cf0cf7b23
EliBrank <eliasbrinkman@gmail.com>
2024-05-14 21:32:57 -0500

    remove blank line for size 0

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -22,8 +22,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print()
     for i in range(size):
         for i in range(size):
             print("#", end="")
