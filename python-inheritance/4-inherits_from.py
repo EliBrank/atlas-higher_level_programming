e9edbe318a14d6e754923abc758242814327ea3f
EliBrank <eliasbrinkman@gmail.com>
2024-05-19 23:44:49 -0500

    create combination check for object that inherits from class but is not of said class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-"""defines the function lookup"""
+"""defines the function inherits_from"""
 
 
-def lookup(obj):
-    """gets all atrributes/methods for input obejct
+def inherits_from(obj, a_class):
+    """checks if specified object is of sub-class of specified class
 
-    Returns: all attributes/methods in list format
+    Returns: True if obj is of sub-class for a_class, else False
     """
 
+    if type(obj) is not a_class and isinstance(obj, a_class):
+        return True
+    else:
+        return False
