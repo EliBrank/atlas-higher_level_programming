ed44138553135b4389955d3325cd92971ddbd05d
EliBrank <eliasbrinkman@gmail.com>
2024-05-20 16:24:29 -0500

    make files executable, make function to read text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""defines the function read_file"""
+
+
+def read_file(filename=""):
+    """opens, reads, prints file specified to stdout"""
+    with open(filename, "r") as f:
+        f_contents = f.read()
+        print(f_contents, end="")
