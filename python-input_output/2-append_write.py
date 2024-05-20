e8fe754b6a3ba9eda30d958a784e51eb03fbcfc1
EliBrank <eliasbrinkman@gmail.com>
2024-05-20 16:44:32 -0500

    adapt write func for append func

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""defines the function append_write"""
+
+
+def append_write(filename="", text=""):
+    """appends string to the end of specified text file
+
+    Returns: number of characters added
+    """
+
+    with open(filename, "a") as f:
+        f_appended = f.write(text)
+        return f_appended
