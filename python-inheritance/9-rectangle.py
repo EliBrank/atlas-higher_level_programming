6429235e8ec09b52bf6db5006529cb939fec2bed
EliBrank <eliasbrinkman@gmail.com>
2024-05-20 14:38:28 -0500

    add print method

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
 
-"""defines the function lookup"""
+"""defines the class Rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-def lookup(obj):
-    """gets all atrributes/methods for input obejct
+class Rectangle(BaseGeometry):
+    """defines Rectangle based on BaseGeometry"""
+    def __init__(self, width, height):
+        """instantiates with width and height
 
-    Returns: all attributes/methods in list format
-    """
+        Args:
+            width: horizontal side length
+            height: vertical side length
+        """
 
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """calculates Rectangle area based on width and height
+
+        Returns: area, i.e. width multiplied by height
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """formats print output to display width and height
+
+        Returns: "[Rectangle] <width>/<height>"
+        """
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
