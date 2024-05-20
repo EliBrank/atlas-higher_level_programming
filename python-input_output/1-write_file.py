ffe7ea1ddd5ed51ded54c124b9041d1989cd1b62
EliBrank <eliasbrinkman@gmail.com>
2024-05-20 16:38:54 -0500

    add file write func

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""defines the function write_file"""
+
+
+def write_file(filename="", text=""):
+    """opens, writes to file specified
+
+    Returns: number of characters written
+    """
+
+    with open(filename, "w") as f:
+        f_written = f.write(text)
+        return f_written
