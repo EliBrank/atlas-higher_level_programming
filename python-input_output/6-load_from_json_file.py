3448016a9708ddb28491865a268a7113279cbc5b
EliBrank <eliasbrinkman@gmail.com>
2024-05-21 11:18:25 -0500

    create load json file to python obj func

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+"""defines the function load_from_json_file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """converts json file contents to python object"""
+
+    with open(filename, "r") as f:
+        return json.load(f)
