9d331580c51dd15f79945b5c7c340138369afcf0
EliBrank <eliasbrinkman@gmail.com>
2024-05-21 10:54:44 -0500

    create json string string to python obj func

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""defines the function from_json_string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """converts json string to python object
+
+    Returns: python object, if possible
+    """
+
+    return json.loads(my_str)
