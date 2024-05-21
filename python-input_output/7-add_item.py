4844908c49d00e1ed76dd4f7e0b24dd596ce8ad1
EliBrank <eliasbrinkman@gmail.com>
2024-05-21 14:19:21 -0500

    create script to add arguments as items in list to json file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""creates, adds to json file list of user args"""
+
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+
+    filename = "add_item.json"
+    f = []
+
+    try:
+        f = load_from_json_file(filename)
+    except FileNotFoundError:
+        pass
+
+    for i in range(1, len(argv)):
+        f.append(argv[i])
+
+    save_to_json_file(f, filename)
