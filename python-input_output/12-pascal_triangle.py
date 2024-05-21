bd451ccd091673d3508a40d1b0d139fc43e2b4ed
EliBrank <eliasbrinkman@gmail.com>
2024-05-21 16:21:18 -0500

    change out var names to shorten line

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -9,19 +9,18 @@ def pascal_triangle(n):
     Returns: list of lists - each list contains row of triangle
     """
 
-    triangle_matrix = []
+    matrix = []
 
     if n <= 0:
-        return triangle_matrix
+        return matrix
 
     for i in range(n):
         row = []
-        triangle_matrix.append(row)
+        matrix.append(row)
         for j in range(i + 1):
             if j == 0 or j == i:
                 row.append(1)
             else:
-                row.append(triangle_matrix[i - 1][j] +
-                triangle_matrix[i - 1][j - 1])
+                row.append(matrix[i - 1][j] + matrix[i - 1][j - 1])
 
-    return triangle_matrix
+    return matrix
