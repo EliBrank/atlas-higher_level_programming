46bed4708d1a50a30e60c775459f24805fdb8f17
EliBrank <eliasbrinkman@gmail.com>
2024-05-26 18:02:14 -0500

    redefine __str__ output in square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,3 +11,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """instantiates Square"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """re-formats Square printout to display class and properties
+
+        Returns: [<class>] (<id>) <x>/<y> - <width>/<height>
+        """
+
+        return (f"[{self.__class__.__name__}] ({self.id}) "
+                f"{self.x}/{self.y} - {self.width}/{self.height}")
