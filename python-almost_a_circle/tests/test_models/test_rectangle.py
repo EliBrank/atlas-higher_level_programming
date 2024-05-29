862bf288e635b91504e34c2ee6c63eb2404710d3
EliBrank <eliasbrinkman@gmail.com>
2024-05-28 21:35:43 -0500

    redo initial rectangle unit tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,23 +9,20 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     """defines unittests for methods in Rectangle class"""
-    def test_init_wh(self):
-        """test width, height"""
-        r = Rectangle(1, 2)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
+    def test_init(self):
+        """test width, height, id"""
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.id, 1)
 
-    def test_init_whx(self):
-        """test width, height, x"""
-        r = Rectangle(1, 2, 3)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 3)
+        r2 = Rectangle(1, 2, 3)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.id, 2)
 
-    def test_init_whxy(self):
-        """test width, height, x, y"""
-        r = Rectangle(1, 2, 3, 4)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 3)
-        self.assertEqual(r.y, 4)
+        r3 = Rectangle(1, 2, 3, 4, 12)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.id, 12)
