b604e2033430b5ee41e26c10d2b7f72c10c125eb
EliBrank <eliasbrinkman@gmail.com>
2024-06-17 17:35:35 +0000

    add label to average column in output

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- retrieves average scores for all rows in second_table
 
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS 'average' FROM second_table;
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- retrieves average scores for all rows in second_table
+
+SELECT COUNT(*) FROM second_table
+WHERE score = ;
