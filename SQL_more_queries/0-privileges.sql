c4a2399e43f99b09d8932b939cb1041b11f502f6
EliBrank <eliasbrinkman@gmail.com>
2024-06-17 16:09:41 -0500

    re-work script to use SHOW GRANTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges for user_0d_1 and user_0d_2 on the server
 
-SELECT * FROM information_schema.user_privileges
-WHERE user = 'user_0d_1' OR user = 'user_0d_2';
+SHOW GRANTS FOR user_0d_1;
+SHOW GRANTS FOR user_0d_2;
