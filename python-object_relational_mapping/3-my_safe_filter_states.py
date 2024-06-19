2d4dc3a53f3ed70ef328e73adbb343de18e3869e
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 21:37:31 +0000

    make filter states function safe

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
 
-# Wait, do you remember the previous task? Did you test "Arizona'; TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '" as an input?
-#
-# guillaume@ubuntu:~/$ ./2-my_filter_states.py root root hbtn_0e_0_usa "Arizona'; TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '"
-# (2, 'Arizona')
-# guillaume@ubuntu:~/$ ./0-select_states.py root root hbtn_0e_0_usa
-# guillaume@ubuntu:~/$ 
-#
-# What? Empty?
-#
-# Yes, it’s an SQL injection to delete all records of a table…
-#
-# Once again, write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
-#
-#     Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)
-#     You must use the module MySQLdb (import MySQLdb)
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by states.id
-#     Results must be displayed as they are in the example below
-#     Your code should not be executed when imported
+"""SQL query - safely retrieves all rows from states table
+
+where name matches argument
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """creates SQL query"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    state_name_searched = argv[4]
+
+    c = db.cursor()
+
+    c.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE %s "
+        "ORDER BY states.id", (state_name_searched,)
+    )
+
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
