8dfbb753011448113c73e3d3ea3e58e6f9b77d5e
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 20:18:37 +0000

    encapsulated function, added main guard

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,18 +6,27 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-)
+def main():
+    """creates SQL query"""
 
-cur = db.cursor()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
 
-cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur = db.cursor()
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
