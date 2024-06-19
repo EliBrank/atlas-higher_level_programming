8aac0b3d7c75bd8a4d67ad5b4e06187d8084b6a4
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 19:47:09 +0000

    ORM script - retrieves all rows from states table

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 
+"""creates SQL query via MySQLdb
 
-Write a script that lists all states from the database hbtn_0e_0_usa:
+retrieves all rows from states table
+"""
 
-    Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
-    You must use the module MySQLdb (import MySQLdb)
-    Your script should connect to a MySQL server running on localhost at port 3306
-    Results must be sorted in ascending order by states.id
-    Results must be displayed as they are in the example below
-    Your code should not be executed when imported
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(
+    host = "localhost",
+    port = 3306,
+    user = argv[1],
+    passwd = argv[2],
+    db = argv[3],
+)
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY states.id")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
