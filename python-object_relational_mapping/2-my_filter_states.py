a9f068665cc2dc719453ee7239f33053cab57d49
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 21:14:36 +0000

    create query constructor that retrieves all rows from states table where name matches arg

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
 
-# Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
-#
-#     Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (no argument validation needed)
-#     You must use the module MySQLdb (import MySQLdb)
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     You must use format to create the SQL query with the user input
-#     Results must be sorted in ascending order by states.id
-#     Results must be displayed as they are in the example below
-#     Your code should not be executed when imported
+"""SQL query - retrieves all rows from states table
+
+where name matches argument
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """creates SQL query"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    state_name_searched = argv[4]
+
+    c = db.cursor()
+
+    c.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' "
+        "ORDER BY states.id".format(state_name_searched)
+    )
+
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
