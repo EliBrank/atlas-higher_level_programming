3418842d18653efc2f34d6ec257f0da9fa241aff
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 05:22:32 +0000

    script - retrieves all genres for the show Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,66 +1,7 @@
+-- retrieves all genres for the show Dexter
 
-
-
-mysql> SHOW TABLES;
-+---------------------------+
-| Tables_in_hbtn_0d_tvshows |
-+---------------------------+
-| tv_genres                 |
-| tv_show_genres            |
-| tv_shows                  |
-+---------------------------+
-
-mysql> SELECT * FROM tv_shows;
-+----+-----------------------+
-| id | title                 |
-+----+-----------------------+
-|  1 | House                 |
-|  2 | Game of Thrones       |
-|  3 | The Big Bang Theory   |
-|  4 | New Girl              |
-|  5 | Silicon Valley        |
-|  6 | Breaking Bad          |
-|  7 | Better Call Saul      |
-|  8 | Dexter                |
-|  9 | Homeland              |
-| 10 | The Last Man on Earth |
-+----+-----------------------+
-
-mysql> SELECT * FROM tv_show_genres;
-+---------+----------+
-| show_id | genre_id |
-+---------+----------+
-|       1 |        1 |
-|       1 |        2 |
-|       2 |        3 |
-|       2 |        1 |
-|       2 |        4 |
-|       3 |        5 |
-|       4 |        5 |
-|       5 |        5 |
-|       6 |        6 |
-|       6 |        1 |
-|       6 |        7 |
-|       6 |        8 |
-|       8 |        6 |
-|       8 |        1 |
-|       8 |        2 |
-|       8 |        7 |
-|       8 |        8 |
-|      10 |        5 |
-|      10 |        1 |
-+---------+----------+
-
-mysql> SELECT * FROM tv_genres;
-+----+-----------+
-| id | name      |
-+----+-----------+
-|  1 | Drama     |
-|  2 | Mystery   |
-|  3 | Adventure |
-|  4 | Fantasy   |
-|  5 | Comedy    |
-|  6 | Crime     |
-|  7 | Suspense  |
-|  8 | Thriller  |
-+----+-----------+
+SELECT tv_genres.name
+FROM tv_genres JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
