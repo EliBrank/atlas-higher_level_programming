508af1eecb5e7ec839542428c0c28e1d98f63726
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 21:58:10 +0000

    create query script to get all cities and their states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python3
 
-# Write a script that lists all cities from the database hbtn_0e_4_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module MySQLdb (import MySQLdb)
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by cities.id
-#     You can use only execute() once
-#     Results must be displayed as they are in the example below
-#     Your code should not be executed when imported
+"""SQL query - retrieves all cities with their states
+
+from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """creates SQL query"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    c = db.cursor()
+
+    c.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id"
+    )
+
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
