6c4507fecf98f3d67dcda1bfb3cd1689a0979a1a
EliBrank <eliasbrinkman@gmail.com>
2024-06-19 03:45:21 +0000

    limit column selection to id, name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- retrieves all cities in California
 
-SELECT * FROM cities WHERE state_id =
+SELECT id, name FROM cities WHERE state_id =
   ( SELECT id FROM states WHERE name = 'California' )
 ORDER BY cities.id;
