de5f6fefe1db339c49cd40ff9e865bc3c9f18552
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 03:16:06 +0000

    adapt model_state_fetch_all func to only print first state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
 
-# Write a script that prints the first State object from the database hbtn_0e_6_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     The state you display must be the first in states.id
-#     You are not allowed to fetch all states from the database before displaying the result
-#     The results must be displayed as they are in the example below
-#     If the table states is empty, print Nothing followed by a new line
-#     Your code should not be executed when imported
-#
-# guillaume@ubuntu:~/$ ./8-model_state_fetch_first.py root root hbtn_0e_6_usa
-# 1: California
-# guillaume@ubuntu:~/$ 
+"""defines the function model_state_fetch_first"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def model_state_fetch_first():
+    """lists first state object for specified database"""
+
+    user = argv[1]
+    password = argv[2]
+    host_name = 'localhost'
+    db_name = argv[3]
+
+    engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+
+    engine = create_engine(
+        engine_base.format(user, password, host_name, db_name)
+    )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        state = session.query(State).order_by(State.id).first()
+        if state:
+            print(f"{state.id}: {state.name}")
+        else:
+            print("Nothing")
+
+
+if __name__ == "__main__":
+    model_state_fetch_first()
