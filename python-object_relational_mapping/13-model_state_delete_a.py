eb2e4bf9230d73fce4128eddb84dd9930a2792bc
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 04:34:56 +0000

    create func to delete state objects containing a

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
 
-# Write a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Your code should not be executed when imported
-#
-# guillaume@ubuntu:~/$ ./13-model_state_delete_a.py root root hbtn_0e_6_usa 
-# guillaume@ubuntu:~/$ ./7-model_state_fetch_all.py root root hbtn_0e_6_usa 
-# 2: New Mexico
-# 4: New York
-# guillaume@ubuntu:~/$ 
+"""defines the function model_state_delete_a"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def model_state_delete_a():
+    """deletes State objects with name containing a"""
+
+    user = argv[1]
+    password = argv[2]
+    host_name = 'localhost'
+    db_name = argv[3]
+
+    engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+
+    engine = create_engine(
+        engine_base.format(user, password, host_name, db_name)
+    )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        states = session.query(State)
+        for state in states:
+            if 'a' in state.name:
+                session.delete(state)
+
+        session.commit()
+
+
+if __name__ == "__main__":
+    model_state_delete_a()
