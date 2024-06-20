1207b94f3df7395aa1d9b98fca6051ee4f2f946e
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 00:21:22 +0000

    create query constructor for finding cities based on state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python3
 
-# Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
-#
-#     Your script should take 4 arguments: mysql username, mysql password, database name and state name (SQL injection free!)
-#     You must use the module MySQLdb (import MySQLdb)
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by cities.id
-#     You can use only execute() once
-#     The results must be displayed as they are in the example below
-#     Your code should not be executed when imported
+"""SQL query - retrieves all cities from the database hbtn_0e_4_usa
+
+data is displayed in a comma-separated list
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """creates SQL query"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    state_name_searched = argv[4]
+
+    c = db.cursor()
+
+    c.execute(
+        "SELECT cities.name "
+        "FROM cities JOIN states ON cities.state_id = states.id "
+        "WHERE BINARY states.name LIKE %s "
+        "ORDER BY cities.id", (state_name_searched,)
+    )
+
+    rows = c.fetchall()
+    list_output = []
+    for row in rows:
+        list_output += row
+
+    print(", ".join(list_output))
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
