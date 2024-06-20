a0f16c2549a324d953ec2be9eb56fd654af072b6
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 01:16:32 +0000

    work on sqlalchemy setup, state class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-# Write a python file that contains the class definition of a State and an instance Base = declarative_base():
-#
-#     State class:
-#         inherits from Base Tips
-#         links to the MySQL table states
-#         class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-#         class attribute name that represents a column of a string with maximum 128 characters and can’t be null
-#     You must use the module SQLAlchemy
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     WARNIN: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine)
+"""defines the function print_square"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition for the State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
