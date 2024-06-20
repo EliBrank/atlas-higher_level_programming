2af9e3bd07c47b78b3cf0984c5196b27db50ff67
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 05:20:26 +0000

    fix imports in city class

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,13 +3,11 @@
 """defines the class City"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base, State
 
 
 class City(Base):
-    """Definition for the State class"""
+    """Definition for the City class"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
