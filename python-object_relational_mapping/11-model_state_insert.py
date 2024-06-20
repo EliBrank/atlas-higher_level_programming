15d8664f7ff702ed2d9f758b29fc82252783f77a
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 04:07:49 +0000

    create func to add Louisiana as state object

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 
-# Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Print the new states.id after creation
-#     Your code should not be executed when imported
-#
-# guillaume@ubuntu:~/$ ./11-model_state_insert.py root root hbtn_0e_6_usa 
-# 6
-# guillaume@ubuntu:~/$ ./7-model_state_fetch_all.py root root hbtn_0e_6_usa 
-# 1: California
-# 2: Arizona
-# 3: Texas
-# 4: New York
-# 5: Nevada
-# 6: Louisiana
-# guillaume@ubuntu:~/$ 
+"""defines the function model_state_insert"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def model_state_insert():
+    """adds new state object (Louisiana) for specified database"""
+
+    user = argv[1]
+    password = argv[2]
+    host_name = 'localhost'
+    db_name = argv[3]
+
+    engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+
+    engine = create_engine(
+        engine_base.format(user, password, host_name, db_name)
+    )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        new_state = State(state='Louisiana')
+        session.add(new_state)
+        session.commit()
+        print(new_state.id)
+
+
+if __name__ == "__main__":
+    model_state_insert()
