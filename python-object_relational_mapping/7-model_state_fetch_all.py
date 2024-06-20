1f666b56af34d2268ac2e6bb890925a0ac8d7d8b
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 03:05:23 +0000

    setup engine, sqlalchemy session for retrieving all state objects from database

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 
-# Write a script that lists all State objects from the database hbtn_0e_6_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by states.id
-#     The results must be displayed as they are in the example below
-#     Your code should not be executed when imported
+"""defines the function model_state_fetch_all"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def model_state_fetch_all():
+    """lists all state objects for specified database"""
+
+    user = argv[1]
+    password = argv[2]
+    host_name = 'mysql'
+    db_name = argv[3]
+
+    engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+
+    engine = create_engine(
+        engine_base.format(user, password, host_name, db_name)
+    )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        states = session.query(State).order_by(State.id)
+        for state in states:
+            print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    model_state_fetch_all()
