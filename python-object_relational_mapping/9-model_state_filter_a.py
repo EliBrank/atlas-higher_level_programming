a21df004209beb8e9981e6482130b838969795c5
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 03:26:47 +0000

    filter out state objects to only return ones with 'a' in name

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 
-# Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
-#
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by states.id
-#     The results must be displayed as they are in the example below
-#     Your code should not be executed when imported
-#
-# guillaume@ubuntu:~/$ ./9-model_state_filter_a.py root root hbtn_0e_6_usa
-# 1: California
-# 2: Arizona
-# 3: Texas
-# 5: Nevada
-# guillaume@ubuntu:~/$ 
+"""defines the function model_state_filter_a"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+
+def model_state_filter_a():
+    """lists first state object for specified database"""
+
+    user = argv[1]
+    password = argv[2]
+    host_name = 'localhost'
+    db_name = argv[3]
+
+    engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+
+    engine = create_engine(
+        engine_base.format(user, password, host_name, db_name)
+    )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        states = session.query(State).order_by(State.id)
+        for state in states:
+            if 'a' in state.name:
+                print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    model_state_filter_a()
