a37dfbe2fa148b88150f047be06b8fed3845f565
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 04:55:12 +0000

    change host name back to localhost

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,7 +14,7 @@ def model_city_fetch_by_state():
 
     user = argv[1]
     password = argv[2]
-    host_name = 'mysql'
+    host_name = 'localhost'
     db_name = argv[3]
 
     engine_base = 'mysql+mysqldb://{}:{}@{}:3306/{}'
