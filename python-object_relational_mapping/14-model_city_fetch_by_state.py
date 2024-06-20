e2448999ee5d3903756576d18f00b28a9cb253e9
EliBrank <eliasbrinkman@gmail.com>
2024-06-20 04:58:23 +0000

    change out place definition

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -28,8 +28,8 @@ def model_city_fetch_by_state():
     Session = sessionmaker(bind=engine)
 
     with Session() as session:
-        location = session.query(City, State).join(State).order_by(City.id)
-        for city, state in location:
+        place = session.query(City, State).join(State).order_by(City.id).all()
+        for city, state in place:
             print(f"{state.name}: ({city.id}) {city.name}")
 
 
