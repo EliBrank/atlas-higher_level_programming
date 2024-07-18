b0667f7abad7d7b9af1cb72b8b50e1046888fbdd
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 20:26:32 +0000

    add missing semicolon

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -9,7 +9,7 @@ const numArgs = args.length;
 if (numArgs === 0) {
   console.log('No argument');
 } else if (numArgs === 1) {
-  console.log('Argument found')
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
