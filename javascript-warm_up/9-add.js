a4e19b8d10ccf90520e270f06bb0f54863549bb5
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:37:49 +0000

    create script to add first two user args

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+const numOne = Number(args[0]);
+const numTwo = Number(args[1]);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(numOne, numTwo);
