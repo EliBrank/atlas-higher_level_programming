fac17d993d3200560ef9726f8ce189ee47acef87
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:51:16 +0000

    create script to calculate factorial of user input number

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+const myNum = Number(args[0]);
+
+function factorial (num) {
+  if (isNaN(num)) {
+    num = 1;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(myNum));
