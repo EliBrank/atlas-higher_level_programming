a80a09d40b1f13a699cddc1edc8ee424f12a058c
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:00:51 +0000

    create script to check for number in user args

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+// gets user args by ignoring first 2 mandatory args
+// e.g. 'node 2-arguments.js' <- these are ignored
+const args = process.argv.slice(2);
+
+const myNum = Number(args[0]);
+
+if (isNaN(myNum)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${myNum}`);
+}
