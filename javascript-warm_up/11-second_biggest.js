9d167e5fe1979a36addd2b505607fb7e50289090
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 22:49:59 +0000

    create script to find second largest number from user input

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+let numBiggest = -Infinity;
+let numSecondBiggest = -Infinity;
+
+function checkSecondBiggest (numArray) {
+
+  if (numArray.length === 0 || numArray.length === 1) {
+    return 0;
+  }
+
+  for (const num of numArray) {
+    if (num > numBiggest) {
+      numSecondBiggest = numBiggest;
+      numBiggest = num;
+    } else if (num > numSecondBiggest && num !== numBiggest) {
+      numSecondBiggest = num;
+    }
+  }
+
+  return numSecondBiggest;
+}
+
+console.log(checkSecondBiggest(args));
