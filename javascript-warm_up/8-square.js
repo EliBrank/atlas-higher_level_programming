28daee9204dbc765ccf1e3d14bda9c7e10a98837
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:29:51 +0000

    add script to print square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+const myNum = Number(args[0]);
+
+if (isNaN(myNum)) {
+  console.log('Missing size');
+} else if (myNum > 0) {
+  for (let i = 0; i < myNum; i++) {
+    for (let j = 0; j < myNum; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+}
