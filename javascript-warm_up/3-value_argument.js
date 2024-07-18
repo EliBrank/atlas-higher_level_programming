8064f914b249e79592c1b400b537c039a861dae2
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 20:33:56 +0000

    add script to print first user arg passed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// gets user args by ignoring first 2 mandatory args
+// e.g. 'node 2-arguments.js' <- these are ignored
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
