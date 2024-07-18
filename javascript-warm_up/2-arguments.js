228cb1c2dd6b18795d3e6a676fd8b66c42cab024
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 20:24:52 +0000

    make script to check user input (args based on argv)

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+// gets user args by ignoring first 2 mandatory args
+// e.g. 'node 2-arguments.js' <- these are ignored
+const args = process.argv.slice(2);
+
+const numArgs = args.length;
+
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found')
+} else {
+  console.log('Arguments found');
+}
