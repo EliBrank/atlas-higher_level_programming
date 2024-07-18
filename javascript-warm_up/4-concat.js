5ccaa99ecdfb73097db5d2cdfa2c95d85dbc5af6
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 20:45:10 +0000

    add script to process user args in template string

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+// gets user args by ignoring first 2 mandatory args
+// e.g. 'node 2-arguments.js' <- these are ignored
+const args = process.argv.slice(2);
+
+console.log(`${args[0]} is ${args[1]}`)
