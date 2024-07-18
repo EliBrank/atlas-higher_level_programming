30844b20adf9350d3b3d3ff7e0a26a1b1fdd19a1
EliBrank <eliasbrinkman@gmail.com>
2024-07-18 21:19:04 +0000

    create script to repeat "C is fun" x number of times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const statement = 'C is fun';
+const args = process.argv.slice(2);
+const myNum = args[0];
+
+if (args[0] === undefined) {
+  console.log('Missing number of occurrences');
+} else if (myNum > 0) {
+  for (let i = 0; i < myNum; i++) {
+	console.log(statement);
+  }
+}
