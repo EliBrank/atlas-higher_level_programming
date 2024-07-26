408f3103befb4c2f9fbb439b8315f54b05cdcf6b
EliBrank <eliasbrinkman@gmail.com>
2024-07-26 18:15:13 +0000

    create script to check status code for input url

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 2) {
+  console.error('please provide at least one argument');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+    process.exit(1);
+  }
+  console.log('code:', response && response.statusCode);
+});
