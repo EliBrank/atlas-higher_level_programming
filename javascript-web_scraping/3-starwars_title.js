b1a1fc5a5b1448bddc834e59b33550d7118fee1c
EliBrank <eliasbrinkman@gmail.com>
2024-07-26 19:22:29 +0000

    create script with star wars film name api call

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 2) {
+  console.error('please provide at least one argument');
+  process.exit(1);
+}
+
+const starWarsFilmNum = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + starWarsFilmNum;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+    process.exit(1);
+  }
+  if (response.statusCode === 200) {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  }
+});
