2049861e5d94dfead5f96bfb763fbd8e44115a02
EliBrank <eliasbrinkman@gmail.com>
2024-07-26 20:53:23 +0000

    add script to check number of tasks completed per user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,33 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length === 2) {
+  console.error('please provide at least one argument');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+    process.exit(1);
+  }
+  const taskCompletion = {};
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed === false) {
+        continue;
+      }
+      if (todo.userId in taskCompletion) {
+        taskCompletion[todo.userId]++;
+      } else {
+        taskCompletion[todo.userId] = 1;
+      }
+    }
+  }
+
+  console.log(taskCompletion);
+});
