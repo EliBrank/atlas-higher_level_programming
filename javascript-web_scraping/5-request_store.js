548e4cff186555469be1f64d9eb241328c9629f1
EliBrank <eliasbrinkman@gmail.com>
2024-07-26 20:14:54 +0000

    create script to read from url and write to file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length === 3) {
+  console.error('please provide at least two arguments');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request
+  .get(url)
+  .on('error', function (err) {
+    console.error(err);
+  })
+  .pipe(fs.createWriteStream(fileName));
