6b71cfb5c197d4048c814ce2c41a84b614251374
EliBrank <eliasbrinkman@gmail.com>
2024-07-26 17:38:25 +0000

    change error handling to print out error object, let readFile handle missing file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,15 +4,14 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-// this check should cover all issues with user input
-if (!fs.existsSync(filePath)) {
-  console.error('please provide a valid file path');
+if (process.argv.length === 2) {
+  console.error('please provide at least one argument');
   process.exit(1);
 }
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-	console.error('error reading the file:', err.message);
+	console.error(err);
 	process.exit(1);
   }
   console.log(data);
